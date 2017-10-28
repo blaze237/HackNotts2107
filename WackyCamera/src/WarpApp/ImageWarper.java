95f965b02db49202ade6e7457e01ab5b6a1e2316
Unknown <psydrw@nottingham.ac.uk>
2017-10-28 18:09:45 +0100

    Add Image to BufferedImage function to transform class

--- WackyCamera/src/WarpApp/ImageWarper.java
+++ WackyCamera/src/WarpApp/ImageWarper.java
@@ -9,20 +9,27 @@ import com.github.sarxos.webcam.WebcamImageTransformer;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
 
+import effects.Grayscale;
+
 public class ImageWarper  implements WebcamImageTransformer {
 
 
 	ImageScanner scanner;
+	Grayscale greyEffect;
 
 	public ImageWarper(ImageScanner scanner) {
 
 		this.scanner = scanner;
+		greyEffect = new Grayscale();
 
 	}
 
 	@Override
 	public BufferedImage transform(BufferedImage img)
 	{
+
+		//return( toBuffImg(greyEffect.apply(scanner.getPixels(img))));
+
 
 		return img;
 		//return Sepia(img);
@@ -32,71 +39,30 @@ public class ImageWarper  implements WebcamImageTransformer {
 
 
 
+	private BufferedImage toBuffImg(Image img)
+	{
+		BufferedImage image = new BufferedImage(img.width,img.height, BufferedImage.TYPE_INT_ARGB);
+
+		for(int r = 0; r < img.height; r++)
+		{
+			for(int c = 0; c < img.width; c++)
+			{
+				image.setRGB(c, r, img.pixels[c][r]);
+			}
+		}
+
+		return image;
+
+	}
 
 
-//	private BufferedImage toGrayscale(BufferedImage img)
-//	{
-//		int width = img.getWidth();
-//		int height = img.getHeight();
-//		int[][] pixels = scanner.getPixels(img);
-//
-//		for(int r = 0; r < height; r++)
-//		{
-//			for(int c = 0; c < width; c++)
-//			{
-//				int p = pixels[c][r];
-//
-//			    int red = (p>>16) & 0xff;
-//			    int green = (p>>8) & 0xff;
-//			    int blue = p & 0xff;
-//			    int avg = (red+green+blue)/3;
-//
-//			    p = (255<<24) | (avg<<16) | (avg<<8) | avg;
-//
-//			    img.setRGB(c, r, p);
-//
-//			}
-//		}
-//
-//		return img;
-//	}
-//
-//
-//	private BufferedImage Sepia(BufferedImage img)
-//	{
-//		int width = img.getWidth();
-//		int height = img.getHeight();
-//		int[][] pixels = scanner.getPixels(img);
-//
-//		for(int r = 0; r < height; r++)
-//		{
-//			for(int c = 0; c < width; c++)
-//			{
-//				int p = pixels[c][r];
-//
-//			    int red = (p>>16) & 0xff;
-//			    int green = (p>>8) & 0xff;
-//			    int blue = p & 0xff;
-//
-//
-//			    int red2 = (int) ((red * .393) + (green *.769) + (blue * .189));
-//			    int green2 = (int) ((red * .349) + (green *.686) + (blue * .168));
-//			    int blue2 =  (int) ((red * .272) + (green *.534) + (blue * .131));
-//
-//			    float boost = 1.5f;
-//
-//			    red2 = (int) Math.min(255, red2*boost);
-//			    green2 = (int) Math.min(255, green2*boost);
-//			    blue2 = (int)Math.min(255, blue2*boost);
-//
-//			    p =  p & 0xff000000 | (red2<<16) | (green2<<8) | blue2;
-//
-//			    img.setRGB(c, r, p);
-//
-//			}
-//		}
-//
-//		return img;
-//	}
+
+
+
+
+
+
+
+
 
 }
