56d988c3509c44cd520d1b0f99a79a5bcd5356a2
psydb3 <psydb3@exmail.nottingham.ac.uk>
2017-10-28 22:22:04 +0100

    added onEffectChanged method

--- WackyCamera/src/gui/GuiMain.java
+++ WackyCamera/src/gui/GuiMain.java
@@ -4,7 +4,7 @@ public class GuiMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Warpy gui = new Warpy();
+		View gui = new View();
 		gui.InitGui();
 	}
 
