fa61a131f5530f964c435f495f255beb125d0cf4
James Kime <psyjk1@exmail.nottingham.ac.uk>
2017-10-28 17:38:01 +0100

    add Image container

---
+++ WackyCamera/src/Image.java
@@ -0,0 +1,14 @@
+
+public class Image {
+
+	public int[][] pixels;
+	public int width;
+	public int height;
+
+	public Image(int[][] pixels, int width, int height) {
+		this.pixels = pixels;
+		this.width = width;
+		this.height = height;
+	}
+
+}
