01d174e98f75a097b534c33765dd0d3a7d8b9dc4
James Kime <psyjk1@exmail.nottingham.ac.uk>
2017-10-28 17:00:49 +0100

    start CircleModifier implementation

--- WackyCamera/src/util/Point.java
+++ WackyCamera/src/util/Point.java
@@ -6,6 +6,13 @@ public class Point {
 		this.y = y;
 	}
 
+	//Translate by a unit vector
+	public Point translate(UnitVec v) { return translate(v, 1.0); }
+	public Point translate(UnitVec v, double dist) { return new Point(x + v.dx() * dist, y + v.dy() * dist); }
+
+	public static double dist2(Point p1, Point p2) { return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2.0); }
+	public static double dist(Point p1, Point p2) { return Math.sqrt(dist2(p1, p2)); }
+
 	public double x;
 	public double y;
 }
