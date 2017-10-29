845fcceb77cc94749f1e3e05e886c979ae99ea90
Unknown <psydrw@nottingham.ac.uk>
2017-10-29 03:27:47 +0000

    New swing theme

--- WackyCamera/src/gui/GuiMain.java
+++ WackyCamera/src/gui/GuiMain.java
@@ -1,11 +1,29 @@
 package gui;
 
+import javax.swing.SwingUtilities;
+
+import com.alee.laf.WebLookAndFeel;
+
 public class GuiMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		View gui = new View();
-		gui.InitGui();
+
+		 SwingUtilities.invokeLater ( new Runnable ()
+	        {
+	            public void run ()
+	            {
+	                // Install WebLaF as application L&F
+	                WebLookAndFeel.install ();
+	                gui.InitGui();
+
+	                // Create you Swing application here
+	                // JFrame frame = ...
+	            }
+	        } );
+
+
 	}
 
 }
