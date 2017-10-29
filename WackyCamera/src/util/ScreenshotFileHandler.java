2694d71457ea3db187ad162d5e57d255bcaaf531
psydb3 <psydb3@exmail.nottingham.ac.uk>
2017-10-29 06:51:25 +0000

    changed screenshot saving to use gui JFileChooser interface

--- WackyCamera/src/util/ScreenshotFileHandler.java
+++ WackyCamera/src/util/ScreenshotFileHandler.java
@@ -1,35 +1,32 @@
 package util;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
+import java.io.EOFException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 
 public class ScreenshotFileHandler {
 	public static boolean saveImage(BufferedImage screenshot) {
-		Random r = new Random(System.currentTimeMillis());
-		int fileNum = r.nextInt(100000);
-		try {
-			File f = new File("Screenshots");
-			if (!f.exists())
-				f.mkdir();
+		JFileChooser saveFile = new JFileChooser(System.getProperty("user.dir") + "/Screenshots/");
+        int ret = saveFile.showSaveDialog(null);
+        if (ret != JFileChooser.APPROVE_OPTION)
+        	return false;
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+        String fName = saveFile.getSelectedFile().getPath();
 
-		try{
-			File out = new File("Screenshots/Sc_" + fileNum + ".png");
-			if (out.exists())
-				out = new File("Screenshots/Sc_" + r.nextInt(100000) + ".png");
-			ImageIO.write(screenshot, "png", out);
+		try
+		{
+			ImageIO.write(screenshot, "png", saveFile.getSelectedFile());
 			return true;
 		}
-		catch (IOException e) {
-			e.printStackTrace();
+		catch(EOFException error)
+		{
+			return false;
+		}
+		catch (Exception i)
+		{
+			i.printStackTrace();
 			return false;
 		}
 	}
